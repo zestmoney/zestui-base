48ff58d29ff460690640d5f709afe547a64fed51
Jay <jay@ip-172-16-3-10.ap-south-1.compute.internal>
2020-01-14 18:03:02 +0530

    test commit

---
+++ base.d.ts
@@ -0,0 +1,17 @@
+export declare const baseStyles: import("lit-element").CSSResult;
+export declare const keyCode: {
+    BACKSPACE: number;
+    TAB: number;
+    RETURN: number;
+    ESC: number;
+    SPACE: number;
+    PAGE_UP: number;
+    PAGE_DOWN: number;
+    END: number;
+    HOME: number;
+    LEFT: number;
+    UP: number;
+    RIGHT: number;
+    DOWN: number;
+    DELETE: number;
+};
